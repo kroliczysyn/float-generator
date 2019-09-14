93bc8c495e35c90db6ca0a0843fe2a33bd92acb9
kroliczysyn <s165497@student.pg.gda.pl>
2019-09-14 20:02:46 +0200

    fixed save to rows in csv file

--- generator.py
+++ generator.py
@@ -4,41 +4,25 @@ import csv
 input_list = []
 output_list = []
 iterations = 1000000
-
-for i in range(iterations):
-    a = random.uniform(0, 1000)
-    b = random.uniform(0, 1000)
-    input_list.append(a)
-    input_list.append(b)
-    c = random.uniform(-3, 3)
-    d = random.uniform(-3, 3)
-    x = a - c
-    y = b - d
-    output_list.append(x)
-    output_list.append(y)
-
 with open('input.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(input_list)
-
-print("Input list: ")
-
-with open('input.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        for i in range(iterations):
-            print(row[i])
+    for i in range(iterations):
+        a = random.uniform(0, 1000)
+        b = random.uniform(0, 1000)
+        c = random.uniform(-1, 1)
+        d = random.uniform(-1, 1)
+        x = a - c
+        y = b - d
+        data_output = (x, y)
+        output_list.append(data_output)
+        data = (a, b)
+        input_list.append(data)
+        spamwriter = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(data)
 
 with open('output.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(output_list)
-
-print("Output list: ")
-
-with open('output.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        for i in range(iterations):
-            print(row[i])
+    for k in range(iterations):
+        data_final_output = (output_list[k][0], output_list[k][1])
+        outputwriter = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        outputwriter.writerow(data_final_output)
